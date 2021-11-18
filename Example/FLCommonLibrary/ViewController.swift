d9c0af3eaa99b8e0469ab1677d7d79a7b9165f4c
dario alarcon <darcon111@hotmail.com>
2021-11-18 09:59:12 -0500

    Initial commit

---
+++ Example/FLCommonLibrary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FLCommonLibrary
+//
+//  Created by darcon111 on 11/18/2021.
+//  Copyright (c) 2021 darcon111. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
